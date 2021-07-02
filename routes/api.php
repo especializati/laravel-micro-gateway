aa8fd9424e0624b12dce9c47aa298c8555f0c7ba
Carlos Ferreira <conta@especializati.com.br>
2021-07-02 15:19:46 -0300

    Get Companies Micro 01

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/companies', function () {
+    $response = Http::withHeaders([
+        'Accept' => 'application/json'
+    ])
+    ->get(config('microservices.available.micro_01.url') . '/companies');
+
+    return response()->json(json_decode($response->body()), $response->status());
+});
 
 Route::get('/', function () {
     return response()->json(['message' => 'success']);
