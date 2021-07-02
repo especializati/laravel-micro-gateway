9dd7141b67e201efe30012b293dff21daa9a2f18
Carlos Ferreira <conta@especializati.com.br>
2021-07-02 15:36:12 -0300

    Create Default Response

---
+++ app/Http/Utils/DefaultResponse.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Utils;
+
+use Illuminate\Http\Client\Response;
+
+class DefaultResponse
+{
+    public function response(Response $response)
+    {
+        $body = json_decode($response->body());
+
+        if ($response->status() !== 200) {
+            return response()->json($body, $response->status());
+        }
+        
+        return [
+            'data' => $this->data($body->data),
+            'meta' => $this->meta($body->meta)
+        ];
+    }
+
+    private function data(array $data)
+    {
+        return collect($data);
+    }
+
+    private function links(array $links)
+    {
+        foreach ($links as $key => $link) {
+            if (isset($link->url) && $link->url != null)  {
+                $links[$key]->url = $this->replacePath($link->url);
+            }
+        }
+
+        return $links;
+    }
+
+    private function meta($meta)
+    {
+        $meta->links = $this->links($meta->links);
+        $meta->path = $this->replacePath($meta->path);
+
+        return $meta;
+    }
+
+    private function replacePath(string $path)
+    {
+        foreach (config('microservices.available') as $key => $micro) {
+            $path = str_replace($micro['url'], config('app.url'), $path);
+        }
+
+        return $path;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,17 @@
 <?php
 
+use App\Http\Utils\DefaultResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/companies', function () {
+Route::get('/companies', function (DefaultResponse $defaultResponse, Request $request) {
     $response = Http::withHeaders([
         'Accept' => 'application/json'
     ])
-    ->get(config('microservices.available.micro_01.url') . '/companies');
+    ->get(config('microservices.available.micro_01.url') . '/companies', $request->all());
 
-    return response()->json(json_decode($response->body()), $response->status());
+    return $defaultResponse->response($response);
 });
 
 Route::get('/', function () {
