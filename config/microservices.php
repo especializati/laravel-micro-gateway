bd7c4407b2266e22ccbd2924b683b9c2d8a3454f
Carlos Ferreira <conta@especializati.com.br>
2021-07-02 15:09:25 -0300

    Config Microservices

---
+++ config/microservices.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'available' => [
+        'micro_01' => [
+            'url' => env('MICRO_01_URL')
+        ],
+
+        'micro_02' => [
+            'url' => env('MICRO_02_URL')
+        ],
+    ]
+
+];
